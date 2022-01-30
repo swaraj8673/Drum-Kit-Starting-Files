bef7af7746b1621f7a1269496f59b4b05f3f2685
Swaraj Behera <swarajbehera.8673@gmail.com>
2022-01-30 22:21:35 +0530

    Add JS functionality to make sound on button click and key press

--- index.js
+++ index.js
@@ -2,11 +2,59 @@
 
 for (var i=0; i<(document.querySelectorAll(".drum")).length; i++) {
 
-  document.querySelectorAll(".drum")[i].addEventListener("click", playTom1);
+  document.querySelectorAll(".drum")[i].addEventListener("click",
+
+  function() {
+
+    var buttonInnerHTML = this.innerHTML;
+    drumClick(buttonInnerHTML);
+  });
 
 }
 
-function playTom1() {
-  var tom1Audio = new Audio("sounds/tom-1.mp3");
-  tom1Audio.play();
+document.addEventListener("keypress",
+
+  function(event) {
+    drumClick(event.key);
+  }
+);
+
+function drumClick(key) {
+
+  switch (key) {
+    case 'w':
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case 'a':
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case 's':
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case 'd':
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case 'j':
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case 'k':
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case 'l':
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+  }
 }
